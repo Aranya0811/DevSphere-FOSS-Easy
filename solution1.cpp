40c5d70497c1d1f555cf9dac909b3c29cf3679a6
Aranya0811 <aarugamer0811@gmail.com>
2026-04-15 16:31:55 +0530

    PROBLEM SOLVED

--- solution1.cpp
+++ solution1.cpp
@@ -4,14 +4,9 @@ using namespace std;
 int main() {
     int t; cin >> t;
     while(t--){
-        long long n, k;
-        cin >> n >> k;
-        long long count = 0, current = 0;
-        while (count < k) {
-            current++;
-            if (current % n != 0) count++;
-        }
-        cout << current << endl;
+    long long n, k;
+    cin >> n >> k;
+    cout << k + (k - 1) / (n - 1) << endl;
     }
-    return 0;
+return 0;
 }
